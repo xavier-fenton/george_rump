51a46300d6b065d0417cbf138a279789147b0bad
Xavier Fenton <xavierfenton99@gmail.com>
2024-07-06 16:21:23 +1200

    Nav Component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ const Nav = () => {
         <div class="font-medium items-center px-[20px] py-[5px] flex justify-between flex-row gap-[5px]">
             <span></span>
             <div>George Rump</div>
-            <div className="text-blue-500 underline">Archive</div>
+            <div className="text-blue-500 underline text-sm"><a href="/archive">archive</a></div>
         </div>
     );
 };
